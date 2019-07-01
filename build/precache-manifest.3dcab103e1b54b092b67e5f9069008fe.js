67069cda8b06f50b98a6bca4845f29845d57ee57
tarasprocishin <taras1procishin@gmail.com>
2019-07-01 21:20:24 +0300

    firstcommit

---
+++ build/precache-manifest.3dcab103e1b54b092b67e5f9069008fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2abfd4e0f63177bfd16085771da95b50",
+    "url": "/statick-list-of-post/index.html"
+  },
+  {
+    "revision": "46bbee53a1162b6f0ec4",
+    "url": "/statick-list-of-post/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/statick-list-of-post/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "46bbee53a1162b6f0ec4",
+    "url": "/statick-list-of-post/static/js/main.d7e42f80.chunk.js"
+  },
+  {
+    "revision": "634080207a7485e44b7b",
+    "url": "/statick-list-of-post/static/js/runtime~main.41e4b733.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import comments from './services/comments';
+import posts from './services/posts';
+import users from './services/users';
+
+function App() {
+
+  console.log(users)
+  return (
+    <div className="App">
+      <h1>Hello Mate</h1>
+    </div>
+  );
+}
+
+export default App;
